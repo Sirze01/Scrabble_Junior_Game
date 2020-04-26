91196688d8cdf72a5b80469147a4c8238cb778f8
Size01 <59342780+Sirze01@users.noreply.github.com>
2020-04-26 22:04:26 +0100

    Bugfix with filename

--- BoardBuilder/BoardBuilder.cpp
+++ BoardBuilder/BoardBuilder.cpp
@@ -9,7 +9,7 @@ int main()
     Board newBoard("test.txt");
     newBoard.show();
     std::cout << newBoard.getIndex("Ak").vLine << ' ' << newBoard.getIndex("Ak").hCollumn << std::endl;
-    newBoard.fileExport("/test_out.txt");
+    newBoard.fileExport("test_out.txt");
 
     std::cout << "Hello world!\n";
     return 0;
