7ecc7a1f9ed3c6b679e06ad6d756f4f46f0a8b1c
Bruno Mendes <61701401+brod56@users.noreply.github.com>
2020-04-26 14:44:16 +0100

    const keyword. no board linking yet.

--- BoardBuilder/Board.h
+++ BoardBuilder/Board.h
@@ -7,9 +7,9 @@ class Board {
 public:
 	Board();
 	Board(std::vector<std::vector<char>> letters);
-	void show();
-	int getIndex(std::string position);
-	void export(std::string filename);
+	void show() const;
+	int getIndex(std::string position) const;
+	void export(std::string filename) const;
 	void import(std::string filename);
 private:
 	std::vector<std::vector<char>> _letters;
--- BoardBuilder/BuildBoard.h
+++ BoardBuilder/BuildBoard.h
@@ -2,9 +2,9 @@
 #include "Board.h"
 class BuildBoard : public Board {
 public:
-	bool inBounds(std::string position, unsigned length, bool horizontal);
-	bool overrides(std::string position, std::string word, bool horizontal);
-	bool isValid(std::string position, std::string word, bool horizontal);
+	bool inBounds(std::string position, unsigned length, bool horizontal) const;
+	bool overrides(std::string position, std::string word, bool horizontal) const;
+	bool isValid(std::string position, std::string word, bool horizontal) const;
 	void write(std::string position, std::string word, bool horizontal);
 };
 
