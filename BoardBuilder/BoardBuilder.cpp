8e35ed7deaf84e00bbc163c1a5ef95940703e478
Bruno Mendes <61701401+brod56@users.noreply.github.com>
2020-04-27 17:34:06 +0100

    Linking error (only Board Builder)

--- BoardBuilder/BoardBuilder.cpp
+++ BoardBuilder/BoardBuilder.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include "Board.h"
-#include "BuildBoard.h"
 
 int main()
 {
     //std::cout << "Hello world!\n";
 
 
-    BuildBoard newBoard("test.txt");
+    Board newBoard("test.txt");
     newBoard.show();
     //std::cout << newBoard.getIndex("Ak").vLine << ' ' << newBoard.getIndex("Ak").hCollumn << std::endl;
     newBoard.fileExport("test_out.txt");
